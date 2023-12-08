97f0fc7c762589b6013d50e07021e8f596915b96
Saulo Moreira <saulo.node@gmail.com>
2023-12-08 11:14:30 -0300

    curso de árabe

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const http = require('http');
 const port = 3000;
 const host = '127.0.0.1';
+const url = require('url')
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type':'text/html'});
-    if(req.url == '/') {
-        res.write('<h1>Welcome</h1>')
-    } else if (req.url == '/canal') {
-        res.write('<h1>Aprenda Hebraico com a Bíblia</h1>');
-    } if (req.url == '/curso') {
-        res.write('<h1>Curso de hebraico</h1>');
-    }
-    res.end();
+    res.write(req.url);
+    const p = url.parse(req.url,true).query;
+    res.write('<br/>' + p.name);
+    res.write('<br/>' + p.course)
+    res.end()
 })
 
-server.listen(port, host, () => {console.log('Server is on...')})
+//http://localhost:3000/?name=saulo&curso=Curso+de+Arabe
+
+server.listen(port, host, () => {console.log('Server running...')})
