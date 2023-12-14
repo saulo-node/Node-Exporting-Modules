706119bee0cc7a422f305ce86455c062f5f8fd10
Saulo Moreira <saulo.node@gmail.com>
2023-12-14 17:57:23 -0300

    Node CRUD MySQL

--- db.js
+++ db.js
@@ -18,7 +18,21 @@ const insereClientes = async(clientes) => {
     const con = await conectar()
     const sql = 'INSERT INTO people (name, age) VALUES (?,?)'
     const valores = [clientes.name, clientes.age]
-    const [linhas] = await con.query(sql,valores)
+    await con.query(sql,valores)
 }
 
-module.exports = {todosClientes, insereClientes}
+const atualizaClientes = async(id, clientes) => {
+    const con = await conectar()
+    const sql = 'UPDATE people SET name=?, age=? WHERE id=?'
+    const valores = [clientes.name, clientes.age,id]
+    await con.query(sql,valores)
+}
+
+const deletarClientes = async(id) => {
+    const con = await conectar()
+    const sql = 'DELETE FROM people WHERE id=?'
+    const valores = [id]
+    await con.query(sql,valores)
+}
+
+module.exports = {todosClientes, insereClientes, atualizaClientes, deletarClientes}
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 (async () => {
     const  db = require('./db')
-    console.log('Inserir novo cliente')
-    const na = "Abdullah"
-    const ag = 18
-    await db.insereClientes({name: na,age: ag})
+    // console.log('Novo cliente inserido')
+    // const na = "Abdullah"
+    // const ag = 18
+    // await db.insereClientes({name: na,age: ag})
+
+    // const id = 2
+    // const na = "Moreach"
+    // const ag = 38
+    // await db.atualizaClientes(id, {name: na,age: ag})
+    // console.log('Cliente' +  'atualizado')
+
+    const id = 3
+    await db.deletarClientes(id)
+    console.log('Cliente' +  id + 'deletado')
 
     console.log('Obter todos os clientes')
     const clientes = await db.todosClientes()
